965abcd929b95d507b5e2c51c5f408e55324ad60
Henrique Rinaldi <henriquergr@hotmail.com>
2023-04-02 18:56:16 -0300

    wip: Add home page

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Today Matches</h1>
+  <ul>
+    <li v-for="match, index in data.matches" :key="index">
+      {{ match.homeTeam.shortName }} {{ match.score.fullTime.home }} X {{ match.score.fullTime.away }} {{ match.awayTeam.shortName }} ({{ match.status }})
+    </li>
+    <!-- <li>matches</li> -->
+  </ul>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+  async setup() {
+    const { data }: { data: any } = await useFetch('https://api.football-data.org/v4/matches/',
+      {
+        method: 'GET',
+        headers: { 'X-Auth-Token': 'c959da3284d94700b0c23cd1686b8f5b' }
+      }
+    )
+
+    return { data }
+  },
+})
+</script>
